f844f1b00b39d2399da11811fdff4c1490cb20b5
ddiazsouto <ddiazsouto@gmail.com>
2021-11-16 09:45:41 +0000

    completed binomial distribution, also includes grap

--- computestats.py
+++ computestats.py
@@ -35,44 +35,57 @@ class Stats:
 
 
 
-class DiscretePdistribution(Stats):
+class Discretedistribution(Stats):
     """ Abstract Data Type that will be reused to generate a p.m.f. """
-    pass
-
-
-
-class stats(Stats):   
-
-        
     
-    def critical_v(probability_function: list, s_level=0.05):
+    def plot(self):
+        
+        pmf = self.probability_distribution
+        title_value = 'critical value =' + str(self.c_value())
+        pd.DataFrame(pmf).plot(kind='bar',  figsize=(9,6), legend=False, title = title_value)
+
+
+
+class Binomial(Discretedistribution):
+    """
+        Implementing a binomial distribution
+    """
+    
+    def __init__(self, n: int, p: int):
+        """ Initiates the class modelling X ~ B(n,p) """
+        
+        self.probability_distribution = self.p_function(n, p)
+        self.cumulative_distribution  = []
+        self.s_level = 0.05
+
+    
+    def c_value(self, s_level=0.05):
         
         cumulative_probability = 0
+        self.s_level = s_level
         
-        for count, i in enumerate(probability_function): 
+        for count, i in enumerate(self.probability_distribution): 
             cumulative_probability = cumulative_probability + i
             
-            if cumulative_probability*2 >= s_level:
+            if cumulative_probability*2 >= 0.05:
                 return count - 1
 
     
-    def p_function(n: int, event_p=0.5, s_level=0.05, graph=False):
+    def p_function(self, n: int, p: float):
         
-        p_x = []        
-        p_each_outcome = float((event_p)**n)
+        pmf = []        
         
-        for possibility in range(0, n+1):
-            
-            ways_to_arrange = stats.C(n, possibility)           
-            p_this_outcome = p_each_outcome * ways_to_arrange
+        for X in range(0, n+1):            
+            ways_to_arrange = Binomial.C(n, X)           
+            P_of_X = ways_to_arrange * ((p)**X) * ((1-p)**(n-X))
               
-            p_x.append(round(p_this_outcome, 4))      
+            pmf.append(round(P_of_X, 4))      
         
-        if graph == True:
-            c_value = 'critical value =' + str(stats.critical_v(p_x))
-            pd.DataFrame(p_x).plot(kind='bar',  figsize=(9,6), legend=False, title = c_value)
-        
-        return p_x
+        self.probability_distribution = pmf
+        return pmf
+    
+    def P(self, X):
+        return self.probability_distribution[X]
     
     
 from random import randint
